facad3915b49234f755a0338512965501ff3af96
Alexandre1832686 <1832686@etu.cchic.ca>
2023-02-22 17:26:18 -0500

    a

---
+++ socket/Perso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace socket
+{
+    public class Perso
+    {
+
+        public string nom { get; set; }
+
+        public int age { get; set; }
+
+        public string sex { get; set; }
+
+        public Perso(string nom, int age, string sex)
+        {
+            this.nom = nom;
+            this.age = age;
+            this.sex = sex;
+        }
+    }
+}
---
+++ socket/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace socket
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // establish the local end point for the socket 
+            IPHostEntry ipHost = Dns.Resolve("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            // create a Tcp/Ip Socket 
+            Socket sListener = new Socket(AddressFamily.InterNetwork,
+                                            SocketType.Stream, ProtocolType.Tcp);
+
+            // bind the socket to the local endpoint and 
+            // listen to the incoming sockets 
+
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                // Start listening for connections 
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection on port {0}", ipEndPoint);
+
+                    // program is suspended while waiting for an incoming connection 
+                    Socket handler = sListener.Accept();
+
+                    string data = null;
+
+                    // we got the client attempting to connect 
+                    while (true)
+                    {
+                        byte[] bytes = new byte[1024];
+
+                        int bytesRec = handler.Receive(bytes);
+
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                        if (data.IndexOf("<theend>") > -1)
+                        {
+                            break;
+                        }
+                    }
+                    int pos = data.IndexOf("<theend>");
+                    if (pos >= 0)
+                    {
+                        // String after founder  
+                        data = data.Remove(pos);
+                        
+                    }
+                       
+                    Perso monPerso = JsonSerializer.Deserialize<Perso>(data);
+
+                    
+                    // show the data on the console 
+                    Console.WriteLine(monPerso.age);
+                    Console.ReadLine();
+                    
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+
+        } // end of Main 
+    }
+}
+
+
+        
